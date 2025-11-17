da18fdf4f3a8ec580d9a9bee2cf2595aa832b09c
Azamatbek <azamatbekromizov@gmail.com>
2025-11-17 11:01:52 +0500

    Amaliy

--- app/src/main/java/com/dev/qurilishdaat/AmaliyList.java
+++ app/src/main/java/com/dev/qurilishdaat/AmaliyList.java
@@ -11,7 +11,7 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class AmaliyList extends AppCompatActivity {
-    private LinearLayout mashgulot1,mashgulot2,mashgulot3,mashgulot4,mashgulot5,mashgulot6,mashgulot7,mashgulot8,mashgulot9;
+    private LinearLayout mashgulot1,mashgulot2,mashgulot3,mashgulot4,mashgulot5,mashgulot6,mashgulot7,mashgulot8,mashgulot9,mashgulot10,mashgulot11,mashgulot12,mashgulot13,mashgulot14,mashgulot15,mashgulot16,mashgulot17,mashgulot18,mashgulot19,mashgulot20,mashgulot21,mashgulot22;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -25,6 +25,20 @@ public class AmaliyList extends AppCompatActivity {
         mashgulot7=findViewById(R.id.mashgulot7);
         mashgulot8=findViewById(R.id.mashgulot8);
         mashgulot9=findViewById(R.id.mashgulot9);
+        mashgulot10=findViewById(R.id.mashgulot10);
+        mashgulot11=findViewById(R.id.mashgulot11);
+        mashgulot12=findViewById(R.id.mashgulot12);
+        mashgulot13=findViewById(R.id.mashgulot13);
+        mashgulot14=findViewById(R.id.mashgulot14);
+        mashgulot15=findViewById(R.id.mashgulot15);
+        mashgulot16=findViewById(R.id.mashgulot16);
+        mashgulot17=findViewById(R.id.mashgulot17);
+        mashgulot18=findViewById(R.id.mashgulot18);
+        mashgulot19=findViewById(R.id.mashgulot19);
+        mashgulot20=findViewById(R.id.mashgulot20);
+        mashgulot21=findViewById(R.id.mashgulot21);
+        mashgulot22=findViewById(R.id.mashgulot22);
+
 
         mashgulot1.setOnClickListener(view -> {
             Intent intent = new Intent(this, FilePages.class);
@@ -69,6 +83,71 @@ public class AmaliyList extends AppCompatActivity {
         mashgulot9.setOnClickListener(view -> {
             Intent intent = new Intent(this, FilePages.class);
             intent.putExtra("pdfFileName", "amaliy9.pdf");
+            startActivity(intent);
+        });
+        mashgulot10.setOnClickListener(view -> {
+            Intent intent = new Intent(this, FilePages.class);
+            intent.putExtra("pdfFileName", "amaliy10.pdf");
+            startActivity(intent);
+        });
+        mashgulot11.setOnClickListener(view -> {
+            Intent intent = new Intent(this, FilePages.class);
+            intent.putExtra("pdfFileName", "amaliy11.pdf");
+            startActivity(intent);
+        });
+        mashgulot12.setOnClickListener(view -> {
+            Intent intent = new Intent(this, FilePages.class);
+            intent.putExtra("pdfFileName", "amaliy12.pdf");
+            startActivity(intent);
+        });
+        mashgulot13.setOnClickListener(view -> {
+            Intent intent = new Intent(this, FilePages.class);
+            intent.putExtra("pdfFileName", "amaliy13.pdf");
+            startActivity(intent);
+        });
+        mashgulot14.setOnClickListener(view -> {
+            Intent intent = new Intent(this, FilePages.class);
+            intent.putExtra("pdfFileName", "amaliy14.pdf");
+            startActivity(intent);
+        });
+        mashgulot15.setOnClickListener(view -> {
+            Intent intent = new Intent(this, FilePages.class);
+            intent.putExtra("pdfFileName", "amaliy15.pdf");
+            startActivity(intent);
+        });
+        mashgulot16.setOnClickListener(view -> {
+            Intent intent = new Intent(this, FilePages.class);
+            intent.putExtra("pdfFileName", "amaliy16.pdf");
+            startActivity(intent);
+        });
+        mashgulot17.setOnClickListener(view -> {
+            Intent intent = new Intent(this, FilePages.class);
+            intent.putExtra("pdfFileName", "amaliy17.pdf");
+            startActivity(intent);
+        });
+        mashgulot18.setOnClickListener(view -> {
+            Intent intent = new Intent(this, FilePages.class);
+            intent.putExtra("pdfFileName", "amaliy18.pdf");
+            startActivity(intent);
+        });
+        mashgulot19.setOnClickListener(view -> {
+            Intent intent = new Intent(this, FilePages.class);
+            intent.putExtra("pdfFileName", "amaliy19.pdf");
+            startActivity(intent);
+        });
+        mashgulot20.setOnClickListener(view -> {
+            Intent intent = new Intent(this, FilePages.class);
+            intent.putExtra("pdfFileName", "amaliy20.pdf");
+            startActivity(intent);
+        });
+        mashgulot21.setOnClickListener(view -> {
+            Intent intent = new Intent(this, FilePages.class);
+            intent.putExtra("pdfFileName", "amaliy21.pdf");
+            startActivity(intent);
+        });
+        mashgulot22.setOnClickListener(view -> {
+            Intent intent = new Intent(this, FilePages.class);
+            intent.putExtra("pdfFileName", "amaliy22.pdf");
             startActivity(intent);
         });
     }
--- app/src/main/java/com/dev/qurilishdaat/ui/home/HomeFragment.java
+++ app/src/main/java/com/dev/qurilishdaat/ui/home/HomeFragment.java
@@ -5,7 +5,6 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
@@ -58,6 +57,11 @@ public class HomeFragment extends Fragment {
         binding.kirish.setOnClickListener(view -> {
             Intent intent = new Intent(getActivity(), FilePages.class);
             intent.putExtra("pdfFileName", "kirish.pdf");
+            startActivity(intent);
+        });
+        binding.dastur.setOnClickListener(view -> {
+            Intent intent = new Intent(getActivity(), FilePages.class);
+            intent.putExtra("pdfFileName", "dastur.pdf");
             startActivity(intent);
         });
         binding.glossary.setOnClickListener(view -> {
